8acad083fef2d3d464a4eb973cdcaa21758df8be
Raymond Camden <raymondcamden@gmail.com>
2020-02-04 13:22:39 -0600

    misc

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 module.exports = function(eleventyConfig) {
 
 
+	eleventyConfig.addFilter("dateFormat", function(value) {
+		return `${value.getMonth()+1}/${value.getDate()}/${value.getFullYear()}`;
+	 });
+
 	eleventyConfig.addCollection("docs", function(collection) {
 		return collection.getFilteredByGlob("docs/*.md").sort((a,b) => {
 			if(a.data.title < b.data.title) return -1;
